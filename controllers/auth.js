4f2318ccac23cad7b2f8dae8b230ffbebdee512b
imNima10 <x1nima1x@gmail.com>
2025-10-08 17:42:38 +0330

    Add local login route and controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const { createAccessToken, createRefreshToken } = require("../utils/token")
 let User = require("../models/user")
 let redis = require("../db/redis")
 const configs = require("../configs")
+let bcrypt = require("bcrypt")
+const { generateUserKey, generateOtp, getOtpDetails, getUserKeyDetails, getOtpRedisPattern, getUserKeyRedisPattern } = require("../utils/auth")
 
 exports.getLogin = async (req, res, next) => {
     return res.render("login")
@@ -23,16 +25,43 @@ exports.login = async (req, res, next) => {
         }
 
         let accessToken = await createAccessToken(existingUser)
-        let refreshToken = await createRefreshToken(existingUser)
-
         res.cookie("access-token", accessToken, {
             httpOnly: true,
             sameSite: "strict",
             maxAge: configs.auth.token.accessToken.expire * 60 * 1000
         })
-
+        
+        let refreshToken = await createRefreshToken(existingUser)
+        await redis.del(`refresh-token:${existingUser._id}`)
         await redis.set(`refresh-token:${existingUser._id}`, refreshToken, "EX", configs.auth.token.refreshToken.expire * 24 * 60 * 60)
+
         return successResponse(res, 200, { msg: "Login successful", data: existingUser })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.otpPage = async (req, res, next) => {
+    let { userKey } = req.params
+
+    return res.render("otp", {
+        userKey
+    })
+}
+
+exports.sendOtp = async (req, res, next) => {
+    try {
+        let { email } = req.body
+        let { expired, remainingTime } = await getOtpDetails(email)
+        if (!expired) {
+            return await errorResponse(res, 429, { msg: `OTP already send,please try again after ${remainingTime}` })
+        }
+        let userKey = await generateUserKey(email)
+        let otp = await generateOtp(email)
+
+        //TODO send OTP email to user
+
+        return res.redirect(`/auth/local/${userKey}`)
     } catch (error) {
         next(error)
     }
--- routers/auth.js
+++ routers/auth.js
@@ -1,7 +1,7 @@
 let express = require("express")
 let router = express.Router()
 
-let { getLogin, login } = require("../controllers/auth")
+let { getLogin, login, otpPage, sendOtp} = require("../controllers/auth")
 const passport = require("passport")
 
 router.get("/login", getLogin)
@@ -9,5 +9,18 @@ router.get("/login", getLogin)
 //? login with google strategy
 router.get("/google", passport.authenticate("google", { session: false, scope: ["profile", "email"] }))
 router.get("/google/callback", passport.authenticate("google", { session: false, failureRedirect: "/auth/login" }), login)
+
+//? login with local strategy
+router.post("/local/verify", (req, res, next) => {
+    passport.authenticate("local", { session: false, failureRedirect: "/auth/login" }, (err, user, info) => {
+        if (err) return next(err);
+        if (!user) return next(info);
+
+        req.user = user
+        return login(req, res, next)
+    })(req, res, next)
+})
+router.get("/local/:userKey", otpPage)
+router.post("/local", sendOtp)
 
 module.exports = router
