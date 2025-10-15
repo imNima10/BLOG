1fefb3490e6f49568bc64cb4822f8dbedf1c427a
imNima10 <x1nima1x@gmail.com>
2025-10-16 00:01:44 +0330

    Add getMe and updateProfile logic with profile.ejs integration

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,10 @@
 let { successResponse, errorResponse } = require("../helpers/responses")
 let { verifyRefreshToken } = require("../utils/token")
+let User = require("../models/user")
 let redis = require("../db/redis")
 let buildError = require("../utils/buildError")
+let path = require("path")
+let fs = require("fs")
 
 exports.logout = async (req, res, next) => {
     try {
@@ -23,3 +26,61 @@ exports.logout = async (req, res, next) => {
         next(error)
     }
 }
+exports.getMe = async (req, res, next) => {
+    try {
+        let user = req.user
+        return res.render("profile", {
+            user
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+exports.updateProfile = async (req, res, next) => {
+    try {
+        let user = req.user;
+        let { username } = req.body;
+        let profile = req.file;
+        let filePath = user.profile
+        if (profile) {
+            filePath = `/images/profiles/${profile.filename}`
+            if (!user.profile.includes("default_profile.png")) {
+                let thePath = path.join(__dirname, "..", "public", user.profile)
+                if (fs.existsSync(thePath)) {
+                    fs.unlink(thePath, (err) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                }
+            }
+        }
+
+        let isUserNameExists = await User.findOne({
+            username
+        })
+
+        if (isUserNameExists && isUserNameExists.email != user.email) {
+            req.flash("validationError", true);
+            return res.redirect("/user/me");
+        }
+
+        let updateUser = await User.findByIdAndUpdate(
+            user._id,
+            {
+                profile: filePath,
+                username: username,
+            },
+            { new: true }
+        );
+
+        if (!updateUser) {
+            throw buildError("user not found", 500);
+        }
+
+        req.flash("success", "update successfully");
+        return res.redirect("/user/me");
+    } catch (error) {
+        next(error);
+    }
+};
--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,15 @@
 let express = require("express")
 let router = express.Router()
 
-let { logout } = require("../controllers/user")
+let { logout, getMe, updateProfile } = require("../controllers/user")
 let { authGuard, roleGuard } = require("../middlewares/guard")
+let { uploaderStorage } = require("../middlewares/uploader")
+let uploader = uploaderStorage({ destination: "profiles" })
 
 router.post("/logout", authGuard, logout)
+
+router.get("/me", authGuard, getMe)
+
+router.post("/update", authGuard, uploader.single("profile"), updateProfile)
 
 module.exports = router
