b464412f150835f48410cb310f0a5d523adfd556
imNima10 <x1nima1x@gmail.com>
2025-10-16 17:05:44 +0330

    Add createPost controller and link to ejs

--- controllers/post.js
+++ controllers/post.js
@@ -16,3 +16,33 @@ exports.getOnePost = async (req, res, next) => {
         next(error)
     }
 }
+exports.createPostPage = async (req, res, next) => {
+    try {
+        return res.render("createPost")
+    } catch (error) {
+        next(error)
+    }
+}
+exports.createPost = async (req, res, next) => {
+    try {
+        let { title, description } = req.body
+        let user = req.user;
+        let cover = req.file;
+        filePath = `/images/covers/${cover.filename}`
+
+        let post = await Post.create({
+            cover: filePath,
+            description,
+            title,
+            user: user._id
+        })
+        if (!post) {
+            req.flash("error", "post dont create");
+            return res.redirect("/post/create");
+        }
+        req.flash("success", "create successfully");
+        return res.redirect(`/post/${post.slug}`);
+    } catch (error) {
+        next(error)
+    }
+}
--- routers/post.js
+++ routers/post.js
@@ -1,8 +1,14 @@
 let express = require("express")
 let router = express.Router()
 
-let { getOnePost } = require("../controllers/post")
+let { getOnePost,createPostPage,createPost } = require("../controllers/post")
 let { authGuard, roleGuard } = require("../middlewares/guard")
+let { uploaderStorage } = require("../middlewares/uploader")
+let uploader = uploaderStorage({ destination: "profiles" })
+
+router.get("/create", authGuard, createPostPage)
+
+router.post("/create", authGuard,uploader.single("cover"), createPost)
 
 router.get("/:slug", authGuard, getOnePost)
 
